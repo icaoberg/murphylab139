067d04f37b82239a391183babff51360e78ae19e
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2013-12-22 21:32:35 +0100

    Updated method to use the default value set by the downloading images script file

--- make_procfile.m
+++ make_procfile.m
@@ -1,6 +1,6 @@
 % This script makes data/procfiles.mat to store yeast images information
 
-% Copyright (C) 2006  Murphy Lab
+% Copyright (C) 2006-2013 Murphy Lab
 % Carnegie Mellon University
 %
 % This program is free software; you can redistribute it and/or modify
@@ -21,10 +21,9 @@
 % For additional information visit http://murphylab.web.cmu.edu or
 % send email to murphy@cmu.edu
 
-
 %Create procfiles.mat which stores yeast image file paths
 %Root directory where the images are stored
-yeastRoot = '/images/yeastGFP';
+yeastRoot = '/images';
 
 if ~exist('yeastRoot')
      fprintf(1,'Please specify the variable yeastRoot in makedata0.m\n\n');
